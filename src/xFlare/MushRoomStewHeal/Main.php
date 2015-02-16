62d047d4a4af3200539d7ae9db8068a01bb66db8
xxFlare <appletreegold@gmail.com>
2015-02-16 02:00:55 -0500

    Better looking code now

--- src/xFlare/MushRoomStewHeal/Main.php
+++ src/xFlare/MushRoomStewHeal/Main.php
@@ -20,24 +20,22 @@ class Main extends PluginBase implements Listener{
             $item = $event->getItem()->getName();
             $config = $this->getConfig();
             $enabled = $config->get("enabled");
-	          if($item == "Mushroom Stew" and $enabled === "yes") { //Checks if item is stew
-              $myhealth = $player->getHealth(); //Gets health
-		          $sethealth = $myhealth + 3.5; //Adds health gain to a variable
-		          $player->setHealth($sethealth); //Sets health
-		          $id = 282; //Mush room stew id is 281
-              $damage = 0;
-              $count = 1;
-		          $soup = new Item($id, $damage, $count);
-	            $player->getInventory()->removeItem($soup);
-	           //---------------------------------\\
-		          $id = 281; //bowl stew id is 281
-              $damage = 0;
-              $count = 1;
-            	//After it removes the soup from the invitory, It gives a bowl
-              $item = new Item($id, $damage, $count); //Defines item
-              $player->getInventory()->addItem($item); //Adds item
-              $sendmessage = $config->get("send-message-when-stew-tapped");
-              if($sendmessage === "yes"){
+	    if($item == "Mushroom Stew" and $enabled === "yes") { //Checks if item is stew
+            	$myhealth = $player->getHealth();
+		$sethealth = $myhealth + 3.5;
+	        $player->setHealth($sethealth);
+	        $id = 282;
+                $damage = 0;
+                $count = 1;
+                $soup = new Item($id, $damage, $count);
+                $player->getInventory()->removeItem($soup);
+		$id = 281;
+                $damage = 0;
+                $count = 1;
+                $item = new Item($id, $damage, $count); //Defines item
+                $player->getInventory()->addItem($item); //Adds item
+                $sendmessage = $config->get("send-message-when-stew-tapped");
+                if($sendmessage === "yes"){
             	   $message = $config->get("message-if-enabled");
             	   $player->sendMessage($message); //Sends message if enabled.
              }
