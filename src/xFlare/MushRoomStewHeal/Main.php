e3092872dd8150939c932241af669ce6c1a77ba7
xxFlare <appletreegold@gmail.com>
2015-02-15 20:04:17 -0500

    Syntax fix

--- src/xFlare/MushRoomStewHeal/Main.php
+++ src/xFlare/MushRoomStewHeal/Main.php
@@ -20,18 +20,18 @@ class Main extends PluginBase implements Listener{
         public function onTouch(PlayerInteractEvent $event){ //Detects taps and such
             $player = $event->getPlayer();
             $item = $event->getItem()->getName();
-		    if($item == "mushroom_stew") { //Checks if item is stew
-		        $myhealth = $player->getHealth(); //Gets health
-		        $sethealth = $myhealth + 3.5; //Adds health gain to a variable
-		        $player->setHeath($setheath); //Sets health
-		        $player->sendMessage("[MushRoomHealer] +3.5 hearts for eating Mush room soup."); //Message to confirm
-		        //Remove soup from invetory | I dont know how to do this yet.
-		        //---------------------------------\\
-		        $id = 281; //Mush room stew id is 281
-            		$damage = 0;
-            		$count = 1;
-            		//After it removes the soup from the invitory, It gives a bowl
-            		$item = new Item($id, $damage, $count); //Defines item
-            		$player->getInventory()->addItem($item); //Added item
-		    }
-        }
+	    if($item == "mushroom_stew") { //Checks if item is stew
+		$myhealth = $player->getHealth(); //Gets health
+		$sethealth = $myhealth + 3.5; //Adds health gain to a variable
+		$player->setHeath($setheath); //Sets health
+		$player->sendMessage("[MushRoomHealer] +3.5 hearts for eating Mush room soup."); //Message to confirm
+	        //Remove soup from invetory | I dont know how to do this yet.
+	        //---------------------------------\\
+		$id = 281; //Mush room stew id is 281
+            	$damage = 0;
+            	$count = 1;
+            	//After it removes the soup from the invitory, It gives a bowl
+            	$item = new Item($id, $damage, $count); //Defines item
+            	$player->getInventory()->addItem($item); //Added item
+		}
+       }
